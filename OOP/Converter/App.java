8daf0def6626bd8bc1830a139dae5140db478487
Cristi-007 <85635855+Cristi-007@users.noreply.github.com>
2022-11-05 19:48:39 +0200

    Delete OOP directory

--- OOP/Converter/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-	public static void main (String[] args) {
-		TempConverter temperature = new TempConverter();
-		temperature.convertToFahr(25);
-		temperature.convertToCelsius(75);
-	}
-}
-
